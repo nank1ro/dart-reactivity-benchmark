2d770eebe66c677cc13c3f6ff5053215dcda5ca2
Seven Du <shiweidu@outlook.com>
2024-12-18 17:41:08 +0800

    feat: print headers

--- lib/run_framework_bench.dart
+++ lib/run_framework_bench.dart
@@ -1,10 +1,12 @@
 import 'cellx_bench.dart';
 // import 'framework_type.dart';
 import 'reactive_framework.dart';
+import 'utils/perf_logging.dart';
 
 void runFrameworkBench(ReactiveFramework framework, {bool? testPullCounts}) {
   // final info =
   //     FrameworkInfo(framework: framework, testPullCounts: testPullCounts);
 
+  logPerfResult(perfReportHeaders());
   cellxBench(framework);
 }
