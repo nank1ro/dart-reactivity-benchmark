9bf8809c38676abde335631a18c959ab2616ef14
Seven Du <shiweidu@outlook.com>
2025-02-20 19:35:38 +0800

    refactor: upgrade solidart 2
    CC @nank1ro

--- frameworks/solidart.dart
+++ frameworks/solidart.dart
@@ -5,28 +5,31 @@ import 'package:reactivity_benchmark/utils/create_computed.dart';
 import 'package:reactivity_benchmark/utils/create_signal.dart';
 
 final class _SolidartReactiveFramework extends ReactiveFramework {
-  const _SolidartReactiveFramework() : super('solidart');
+  const _SolidartReactiveFramework() : super('solidart(2.0-dev)');
 
   @override
   Computed<T> computed<T>(T Function() fn) {
-    final inner = solidart.Computed(() => fn());
-    return createComputed(inner.call);
+    final computed = solidart.Computed(fn);
+    return createComputed(() => computed.value);
   }
 
   @override
   void effect(void Function() fn) {
-    solidart.Effect((_) => fn());
+    solidart.Effect(fn);
   }
 
   @override
   Signal<T> signal<T>(T value) {
-    final inner = solidart.Signal(value);
-    return createSignal(inner.call, inner.set);
+    final signal = solidart.Signal(value);
+    return createSignal(
+      () => signal.value,
+      (value) => signal.value = value,
+    );
   }
 
   @override
   void withBatch<T>(T Function() fn) {
-    fn();
+    solidart.batch(fn);
   }
 
   @override
@@ -35,5 +38,7 @@ final class _SolidartReactiveFramework extends ReactiveFramework {
 
 void main() {
   solidart.SolidartConfig.devToolsEnabled = false;
+  solidart.SolidartConfig.trackPreviousValue = false;
+  solidart.SolidartConfig.equals = true;
   runFrameworkBench(const _SolidartReactiveFramework());
 }
